ad81122f30f8d49320fea8ef72aded195f3788cd
Ankit Kumar <ankitiit263@gmail.com>
2024-03-19 23:12:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,43 +1,63 @@
 import streamlit as st
-import pickle
-import numpy as np
 import pandas as pd
+import numpy as np
+import pickle
+from sklearn.linear_model import LinearRegression
 
-# Load the model
-model = pickle.load(open('LinearRegressionModel.pkl','rb'))
+# Load the dataset
+@st.cache
+def load_data():
+    return pd.read_csv("car_data.csv")
 
-# Load the DataFrame
-df = pickle.load(open('df.pkl','rb'))  # Load the DataFrame
+data = load_data()
 
-st.title("Car Price Predictor")
+st.title("Car Price Prediction App")
 
-# Select car name
-name = st.selectbox('Car Name', df['name'].unique())
+# Create features and target variable
+X = data[['Year', 'Present_Price', 'Kms_Driven', 'Owner', 'Fuel_Type', 'Seller_Type', 'Transmission']]
+y = data['Selling_Price']
 
-# Select car company
-company = st.selectbox('Company', df['company'].unique())
+# Perform one-hot encoding on categorical variables
+X = pd.get_dummies(X, drop_first=True)
 
-# Select year
-year = st.number_input('Year', min_value=1900, max_value=2024, step=1, value=2010)
+# Train the model
+model = LinearRegression()
+model.fit(X, y)
 
-# Enter kilometers driven
-kms_driven = st.number_input('Kilometers Driven')
+# Save the model to disk
+filename = 'LinearRegressionModel.pkl'
+pickle.dump(model, open(filename, 'wb'))
 
-# Select fuel type
-fuel_type = st.selectbox('Fuel Type', ['Petrol', 'Diesel', 'CNG'])
+# Load the model from disk
+model = pickle.load(open(filename, 'rb'))
 
-if st.button('Predict Price'):
-    # Preprocess input
-    # You may need to encode categorical variables and scale numerical features here based on your preprocessing steps
+# Take input from user
+year = st.slider("Select the Year of the Car", min_value=2003, max_value=2019, step=1)
+present_price = st.slider("Select the Present Price (in Lakhs)", min_value=0.5, max_value=30.0, step=0.5)
+kms_driven = st.slider("Select the Kilometers Driven", min_value=5000, max_value=50000, step=1000)
+owner = st.slider("Select the Number of Previous Owners", min_value=0, max_value=2, step=1)
 
-    # Create query array
-    query = np.array([[name, company, year, kms_driven, fuel_type]])
-    query_df = pd.DataFrame(query, columns=['name', 'company', 'year', 'kms_driven', 'fuel_type'])
+fuel_type = st.selectbox("Select Fuel Type", ['Petrol', 'Diesel'])
+if fuel_type == 'Petrol':
+    fuel_type_petrol = 1
+    fuel_type_diesel = 0
+else:
+    fuel_type_petrol = 0
+    fuel_type_diesel = 1
 
-    # Assuming you have preprocessing steps to transform the query data similar to how it was done during training
-    # You might need to encode categorical variables and scale numerical features accordingly
+seller_type = st.selectbox("Select Seller Type", ['Individual', 'Dealer'])
+if seller_type == 'Individual':
+    seller_type_individual = 1
+else:
+    seller_type_individual = 0
 
-    # Make prediction
-    prediction = model.predict(query_df)
+transmission = st.selectbox("Select Transmission Type", ['Manual', 'Automatic'])
+if transmission == 'Manual':
+    transmission_manual = 1
+else:
+    transmission_manual = 0
 
-    st.title("The predicted price of this car configuration is $" + str(prediction[0]))
+# Make prediction
+prediction = model.predict([[year, present_price, kms_driven, owner, fuel_type_petrol, fuel_type_diesel, seller_type_individual, transmission_manual]])
+
+st.write(f"The estimated selling price of the car is {prediction[0]} Lakhs.")
